62d85de9e70c79ee651d9f4776f689a143bb11fb
csgarcia612 <csgarcia612@comcast.net>
2019-01-21 11:46:11 -0700

    testing svg textpath for safari use

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,30 +2,57 @@ import React, { Component } from "react";
 
 export default class Homepage extends Component {
   render() {
+    let isSafari =
+      /Safari/.test(navigator.userAgent) &&
+      /Apple Computer, Inc/.test(navigator.vendor);
     return (
       <div className="homepageContainer">
         <div className="welcomeBannerContainer">
-          <div className="gifContainer">
-            <svg viewBox="0 0 500 500">
-              <path
-                id="curve"
-                // d="M50,250 a1,1 0 1,1 400,0"
-                // d="M25,250 a1,1 0 1,1 450,0"
-                // d="M40,250 a1,1 0 1,1 425,0"
-                d="M45,250 a1,1 0 1,1 410,0"
+          {isSafari ? (
+            <div className="gifContainer">
+              <svg viewBox="0 0 500 500">
+                <path
+                  id="curve"
+                  // d="M50,250 a1,1 0 1,1 400,0"
+                  // d="M25,250 a1,1 0 1,1 450,0"
+                  // d="M40,250 a1,1 0 1,1 425,0"
+                  d="M45,250 a1,1 0 1,1 410,0"
+                />
+                <text className="curvedText" width="500">
+                  <textPath xlinkhref="#curve">
+                    * Please Don't Shop . . . Just Adopt *
+                  </textPath>
+                </text>
+              </svg>
+              <img
+                className="dogGif"
+                src="../images/smiling-dog.gif"
+                alt="Smiling dog reads text rendered above"
               />
-              <text className="curvedText" width="500">
-                <textPath href="#curve">
-                  * Please Don't Shop . . . Just Adopt *
-                </textPath>
-              </text>
-            </svg>
-            <img
-              className="dogGif"
-              src="../images/smiling-dog.gif"
-              alt="Smiling dog reads text rendered above"
-            />
-          </div>
+            </div>
+          ) : (
+            <div className="gifContainer">
+              <svg viewBox="0 0 500 500">
+                <path
+                  id="curve"
+                  // d="M50,250 a1,1 0 1,1 400,0"
+                  // d="M25,250 a1,1 0 1,1 450,0"
+                  // d="M40,250 a1,1 0 1,1 425,0"
+                  d="M45,250 a1,1 0 1,1 410,0"
+                />
+                <text className="curvedText" width="500">
+                  <textPath href="#curve">
+                    * Please Don't Shop . . . Just Adopt *
+                  </textPath>
+                </text>
+              </svg>
+              <img
+                className="dogGif"
+                src="../images/smiling-dog.gif"
+                alt="Smiling dog reads text rendered above"
+              />
+            </div>
+          )}
         </div>
       </div>
     );
